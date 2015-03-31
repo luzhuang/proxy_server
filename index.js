c05c7ffbcfe6f469f6ae2719a31de5d7a8a64318
陆庄 <wz84888@alibaba-inc.com>
2015-03-31 11:10:12 +0800

    aaa

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var test = require('./lib/server.js');
+var cluster = require('cluster');
+
 test.createProxyServer(8102);
 test.setSocks5Port(1888);
 // test.setRouter({".*" : "127.0.0.1:8084"});
--- test2.js
+++ test2.js
@@ -1,34 +1,15 @@
-var net = require('net');
-var https = require('https');
-net.createServer(function(client){
-	var buffer = new Buffer(0);
-	var host,port;
-	client.on('data',function(data){
-		buffer = Buffer.concat([buffer,data]);
-		var header = data.toString().split(' ');
-		client.removeAllListeners('data');
-		console.log(data.toString());
-		if (header[0] == 'CONNECT'){
-			host = header[1].split(':')[0];
-			port = header[1].split(':')[1];
-		}
-		console.log(host,port);
-		var server = net.createConnection(port,host);
-		server.on('data',function(chunk){ console.log('+',chunk);client.write(chunk); })
-		client.on('data',function(chunk) {console.log('-',chunk.toString());server.write(chunk); });
-		if (header[0] == 'CONNECT'){
-		// 	console.log(1);
-			client.write(new Buffer("HTTP/1.1 200 Connection established\r\nConnection: close\r\n\r\n"));
-		}
-		// 	console.log(data.toString());
-		// 	server.write(data);
-		// }
-			
-		
-	});
-}).listen(8102);
-
-process.on('uncaughtException', function(err){
-    console.log("\nError!!!!");
-    console.log(err);
-});
+var http = require('http'),
+     numCPUs = require('os').cpus().length;
+     cp = require('child_process'), 
+     net = require('net');
+ var workers = [];
+ for (var i = 0; i < numCPUs; i++) {
+     workers.push(cp.fork('app.js', ['normal']));
+ }
+ 
+ net.createServer(function(s) {
+     s.pause();
+     var worker = worker.shift();
+     worker.send('c',s);
+     workers.push(worker);
+ }).listen(8000);
