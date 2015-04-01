e4007600e3e6630944896cbc406228148837dc11
陆庄 <wz84888@alibaba-inc.com>
2015-04-01 22:04:34 +0800

    first

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-var test = require('./lib/server.js');
-// var test = require('./lib/socks5.js');
+// var test = require('./lib/server.js');
+var test = require('./lib/socks5.js');
 
 test.setConfig({
 	http_server_port : 8888,
 	https_server_port : 8889,
 });
-test.createProxyServer(8102);
-console.log(test.getCAPath());
+test.use(function(req,res){
+	console.log(req.url);
+	return true;
+}).use(function(req,res){
+	console.log(req.headers);
+	return true;
+}).listen(8102);
