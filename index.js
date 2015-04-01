d3d6e393253037d77a58b9410684f22003d094df
陆庄 <wz84888@alibaba-inc.com>
2015-04-01 15:02:28 +0800

    certpath

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// var test = require('./lib/server.js');
 var test = require('./lib/server.js');
+// var test = require('./lib/socks5.js');
 
 test.setConfig({
 	http_server_port : 8888,
 	https_server_port : 8889,
-	download_cert_port : 8890
 });
 test.createProxyServer(8102);
+console.log(test.getCAPath());
